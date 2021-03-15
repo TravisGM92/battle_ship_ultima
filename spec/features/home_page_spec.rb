10e1980c232b2e54eb1904f8713422dc84ab2d6e
Travis <mckinstrytg1@gmail.com>
2021-03-15 17:14:45 -0600

    Creates about page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Home page' do
    it 'exists' do
      visit('/')
+     expect(page).to have_link('About')
+  end
+  it 'has an About link that redirects to /about' do
+    visit('/')
+    click_link('About')
+    expect(current_path).to eq('/about')
   end
 end
